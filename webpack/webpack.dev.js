851b8ff7d5e114d44b9b824c57f6dd79a86d5f2a
Alexandra Bandarenka <alexandrabandarenka@nav.no>
2020-03-18 14:09:49 +0100

    endringer i output

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,8 +4,8 @@ const path = require('path');
 const merge = require('webpack-merge');
 const commonDevAndProd = require('./webpack.common.dev_and_prod.js');
 
-const ROOT_DIR = path.resolve(__dirname, '../src/client');
-const APP_DIR = path.resolve(ROOT_DIR, 'app');
+const ROOT_DIR = path.resolve(__dirname, '../public');
+const APP_DIR = path.resolve(__dirname, '../src/client/app');
 
 const config = {
   mode: 'development',
