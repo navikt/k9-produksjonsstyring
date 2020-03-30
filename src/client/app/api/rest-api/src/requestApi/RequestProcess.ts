07fa41b1de012e66d535e7fec205f2a063b2913d
Alexandra Bandarenka <alexandrabandarenka@nav.no>
2020-03-30 10:07:54 +0200

    fikser tester

--- src/client/app/api/rest-api/src/requestApi/RequestProcess.ts
+++ src/client/app/api/rest-api/src/requestApi/RequestProcess.ts
@@ -1,7 +1,7 @@
 import EventType from './eventType';
 import asyncPollingStatus from './asyncPollingStatus';
 import { HttpClientApi } from '../HttpClientApiTsType';
-import { Response } from './ResponseTsType';
+import { Response, SuccessResponse } from './ResponseTsType';
 import { RequestAdditionalConfig } from '../RequestAdditionalConfigTsType';
 import TimeoutError from './error/TimeoutError';
 import RequestErrorEventHandler from './error/RequestErrorEventHandler';
@@ -12,11 +12,22 @@ const CANCELLED = 'INTERNAL_CANCELLATION';
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const hasLocationAndStatusDelayedOrHalted = responseData => responseData.location
-    && (responseData.status === asyncPollingStatus.DELAYED || responseData.status === asyncPollingStatus.HALTED);
+const getRestMethod = (httpClientApi: HttpClientApi, restMethodString: string) => {
+  switch (restMethodString) {
+    case 'POST':
+      return httpClientApi.post;
+    case 'PUT':
+      return httpClientApi.put;
+    default:
+      return httpClientApi.get;
+  }
+};
 
-type Notify = (eventType: keyof typeof EventType, data?: any, isPolling?: boolean) => void;
-type NotificationEmitter = (eventType: keyof typeof EventType, data?: any) => void;
+const hasLocationAndStatusDelayedOrHalted = responseData => responseData.location && (responseData.status === asyncPollingStatus.DELAYED
+    || responseData.status === asyncPollingStatus.HALTED);
+
+type Notify = (eventType: keyof typeof EventType, data?: any) => void
+type NotificationEmitter = (eventType: keyof typeof EventType, data?: any) => void
 
 interface ResponseDataLink {
   href: string;
@@ -46,16 +57,10 @@ class RequestProcess {
 
   notify: Notify = () => undefined;
 
-  isCancelled = false;
+  isCancelled: boolean = false;
 
-  isPollingRequest = false;
-
-  constructor(
-      httpClientApi: HttpClientApi,
-      restMethod: (url: string, params: any, responseType?: string) => Promise<Response>,
-      path: string,
-      config: RequestAdditionalConfig,
-  ) {
+  constructor(httpClientApi: HttpClientApi, restMethod: (url: string, params: any, responseType?: string) => Promise<Response>,
+              path: string, config: RequestAdditionalConfig) {
     this.httpClientApi = httpClientApi;
     this.restMethod = restMethod;
     this.path = path;
@@ -65,9 +70,9 @@ class RequestProcess {
 
   setNotificationEmitter = (notificationEmitter: NotificationEmitter) => {
     this.notify = notificationEmitter;
-  };
+  }
 
-  execLongPolling = async (location: string, pollingInterval = 0, pollingCounter = 0): Promise<Response> => {
+  execLongPolling = async (location: string, pollingInterval: number = 0, pollingCounter: number = 0): Promise<Response> => {
     if (pollingCounter === this.maxPollingLimit) {
       throw new TimeoutError(location);
     }
@@ -95,14 +100,23 @@ class RequestProcess {
     return statusOrResultResponse;
   };
 
-  execute = async (
-      path: string,
-      restMethod: (path: string, params?: any) => Promise<Response>,
-      params: any,
-  ): Promise<Response> => {
+  execLinkRequests = async (responseData: {links: ResponseDataLink[]}) => {
+    const requestList = responseData.links
+        .map(link => () => this.execute(// eslint-disable-line no-use-before-define
+            link.href, getRestMethod(this.httpClientApi, link.type), link.requestPayload,
+        )
+            .then((response: SuccessResponse) => Promise.resolve({ [link.rel]: response.data })));
+
+    // TODO (TOR) Må kunna konfigurera om ein skal feila om eitt av kalla feilar. Og kva med logging?
+    return Promise.all([Promise.resolve(responseData), ...requestList.map(request => request())])
+        .then(allResponses => (this.config.addLinkDataToArray
+            ? allResponses.reduce((acc, rData) => (rData.links ? acc : acc.concat(Object.values(rData)[0])), [])
+            : allResponses.reduce((acc, rData) => ({ ...acc, ...rData }), {})));
+  }
+
+  execute = async (path: string, restMethod: (path: string, params?: any) => Promise<Response>, params: any): Promise<Response> => {
     let response = await restMethod(path, params);
     if ('status' in response && response.status === HTTP_ACCEPTED) {
-      this.isPollingRequest = true;
       try {
         response = await this.execLongPolling(response.headers.location);
       } catch (error) {
@@ -117,14 +131,19 @@ class RequestProcess {
         }
       }
     }
+    const responseData = response && 'data' in response && response.data;
+    if (this.config.fetchLinkDataAutomatically && responseData && responseData.links && responseData.links.length > 0) {
+      // TODO (TOR) execLinkRequests returnerar no responseData.
+      response = await this.execLinkRequests(responseData);
+    }
     return response;
-  };
+  }
 
   cancel = () => {
     this.isCancelled = true;
-  };
+  }
 
-  run = async (params: any): Promise<{ payload: any }> => {
+  run = async (params: any): Promise<{payload: any}> => {
     this.notify(EventType.REQUEST_STARTED);
 
     try {
@@ -133,18 +152,16 @@ class RequestProcess {
         return { payload: CANCELLED };
       }
 
-      const responseData = response.data;
-      this.notify(EventType.REQUEST_FINISHED, responseData, this.isPollingRequest);
-      return responseData ? { payload: responseData } : { payload: undefined };
+      // TODO (TOR) Bør unngå denne testen ved å sikre at ein alltid får det same tilbake fra execute
+      const responseData = 'data' in response && response.data !== undefined ? response.data : response;
+
+      this.notify(EventType.REQUEST_FINISHED, responseData);
+      return responseData ? { payload: responseData } : { payload: [] };
     } catch (error) {
-      const { response } = error;
-      if (response && response.status === 401 && response.headers && response.headers.location) {
-        window.location = response.headers.location;
-      }
-      new RequestErrorEventHandler(this.notify, this.isPollingRequest).handleError(error);
+      new RequestErrorEventHandler(this.notify).handleError(error);
       throw error;
     }
-  };
+  }
 }
 
 export default RequestProcess;
