8084b96e1ea13440cd00cd252fe4ce3848079c5c
Alexandra Bandarenka <alexandrabandarenka@nav.no>
2020-04-03 15:56:00 +0200

    fjernet build fra gitignore

---
+++ src/build/scripts/envSettings.js
@@ -0,0 +1,12 @@
+const fsExtra = require('fs-extra');
+
+function createEnvSettingsFile(settingsFile) {
+    fsExtra.ensureFile(settingsFile).then((f) => {
+        fsExtra.writeFileSync(
+            settingsFile,
+            `window.appSettings = {AUTH_PROXY_URL: '${process.env.AUTH_PROXY_URL}'};`
+        );
+    });
+}
+
+module.exports = createEnvSettingsFile;
