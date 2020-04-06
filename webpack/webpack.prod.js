27af55664eb13696223a3fff097be4b6cad74906
alexandraban <alexandrabandarenka@nav.no>
2020-04-06 14:32:46 +0200

    mer endringer

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -21,7 +21,7 @@ const config = {
   output: {
     filename: 'bundle-[hash].js',
     path: path.resolve(__dirname, '../dist'),
-    publicPath: 'public',
+    publicPath: '/',
   },
 
   plugins: [
