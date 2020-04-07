e58056c81642aa12fe2e8a0f9332fb4d8fda2eea
alexandraban <alexandrabandarenka@nav.no>
2020-04-07 08:38:51 +0200

    forenklet server.js

--- server.js
+++ server.js
@@ -1,46 +1,17 @@
 const express = require('express');
 const path = require('path');
-const mustacheExpress = require('mustache-express');
-const Promise = require('promise');
-const compression = require('compression');
-const helmet = require('helmet');
-const createEnvSettingsFile = require('./src/build/scripts/envSettings');
-
 const server = express();
-server.use(helmet());
-server.use(compression());
-server.set('views', `${__dirname}/dist`);
-server.set('view engine', 'mustache');
-server.engine('html', mustacheExpress());
-server.use(express.static(`${__dirname}/public`));
+server.use(express.static(path.join(__dirname, 'dist')));
 
-createEnvSettingsFile(path.resolve(`${__dirname}/dist/js/settings.js`));
+const PORT = process.env.PORT || 8080;
 
-const renderApp = () =>
-    new Promise((resolve, reject) => {
-        server.render('index.html', {}, (err, html) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(html);
-            }
-        });
-    });
+server.get('/isAlive', (req, res) => res.sendStatus(200));
+server.get('/isReady', (req, res) => res.sendStatus(200));
 
-const startServer = (html) => {
-    server.get('/isAlive', (req, res) => res.sendStatus(200));
-    server.get('/isReady', (req, res) => res.sendStatus(200));
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-    server.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-    });
+server.listen(PORT);
 
-    const port = process.env.PORT || 8030;
-    server.listen(port, () => {
-        console.log(`App listening on port: ${port}`);
-    });
-};
-
-const logError = (errorMessage, details) => console.log(errorMessage, details);
-
-renderApp().then(startServer, (error) => logError('Failed to render app', error));
+console.log(`Started express server at port ${PORT}`);
