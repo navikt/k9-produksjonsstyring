0c9ce326b0c54c42966c223bdf99df7bee4c3afb
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-04-07 12:10:09 +0200

    Direkte url

--- src/client/app/api/rest-api/src/axios/axiosRestMethods.ts
+++ src/client/app/api/rest-api/src/axios/axiosRestMethods.ts
@@ -27,9 +27,9 @@ const defaultPostHeaders = {
 };
 
 const get = axiosInstance => (url: string, params: any, responseType: string = 'json') => {
-  url = `https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api/${url}`;
+  const urlRedir = `https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api/${url}`;
   return cancellable(axiosInstance, {
-    url,
+    urlRedir,
     params,
     responseType,
     method: 'get',
