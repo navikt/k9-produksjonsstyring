97ead196157aca397a0c6f0c9aa192facafb7c3a
alexandraban <alexandrabandarenka@nav.no>
2020-04-07 13:35:47 +0200

    fjernet etra slash

--- src/client/app/api/rest-api/src/axios/axiosRestMethods.ts
+++ src/client/app/api/rest-api/src/axios/axiosRestMethods.ts
@@ -27,7 +27,7 @@ const defaultPostHeaders = {
 };
 
 const get = axiosInstance => (url: string, params: any, responseType: string = 'json') => {
-  const urlRedir = url ? `https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api/${url}` : null;
+  const urlRedir = url ? `https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api${url}` : null;
   return cancellable(axiosInstance, {
     url: urlRedir,
     params,
