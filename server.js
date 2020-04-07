45c01a156d3cd13bdf905122d1540086988bfeb4
alexandraban <alexandrabandarenka@nav.no>
2020-04-07 08:52:21 +0200

    riktig portnummer

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const server = express();
 server.use(express.static(path.join(__dirname, 'dist')));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8030;
 
 server.get('/isAlive', (req, res) => res.sendStatus(200));
 server.get('/isReady', (req, res) => res.sendStatus(200));
