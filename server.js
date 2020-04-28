b1f74e0f87ef3a3f0ef75869824c7c844491fdef
alexandraban <alexandrabandarenka@nav.no>
2020-04-28 10:24:29 +0200

    env settings

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const server = express();
+const createEnvSettingsFile = require('./src/build/scripts/envSettings');
 server.use(express.static(path.join(__dirname, 'dist')));
 
 const PORT = process.env.PORT || 8030;
+
+createEnvSettingsFile(path.resolve(`${__dirname}/dist/js/settings.js`));
 
 server.get('/isAlive', (req, res) => res.sendStatus(200));
 server.get('/isReady', (req, res) => res.sendStatus(200));
--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -4,7 +4,7 @@ function createEnvSettingsFile(settingsFile) {
     fsExtra.ensureFile(settingsFile).then((f) => {
         fsExtra.writeFileSync(
             settingsFile,
-            `window.appSettings = {AUTH_PROXY_URL: '${process.env.AUTH_PROXY_URL}'};`
+            `window.appSettings = {AUTH_PROXY_URL: '${process.env.AUTH_PROXY_URL}'};`,
         );
     });
 }
