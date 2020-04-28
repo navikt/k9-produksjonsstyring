c40eac7c4cf3ad98b5dd9cb24b9e939523257da4
alexandraban <alexandrabandarenka@nav.no>
2020-04-28 12:15:12 +0200

    mer logging

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const server = express();
+
 server.use(express.static(path.join(__dirname, 'dist')));
 const PORT = process.env.PORT || 8030;
-const ENVIRONMENT = process.env.NODE_ENV;
-const CLUSTER_NAME = process.env.CLUSTER;
-const ENV = process.env;
 
+const CLUSTER = process.env.NAIS_CLUSTER_NAME;
 
 server.get('/isAlive', (req, res) => res.sendStatus(200));
 server.get('/isReady', (req, res) => res.sendStatus(200));
@@ -19,9 +18,8 @@ server.get('/avdelingsleder', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+console.log(`cluster ${CLUSTER}`);
 server.listen(PORT);
 
 console.log(`Started express server at port ${PORT}`);
-console.log(`Environment name: ${ENVIRONMENT}`);
-console.log(`Cluster name: ${CLUSTER_NAME}`);
-console.log(`LIste: ${ENV}`);
+
