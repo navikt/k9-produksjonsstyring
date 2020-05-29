df445efa7c196910743b443fb50d9471bbdeaa85
alexandraban <alexandrabandarenka@nav.no>
2020-05-29 12:40:29 +0200

    legger på slash

--- src/client/app/api/rest-api/src/axios/initRestMethods.ts
+++ src/client/app/api/rest-api/src/axios/initRestMethods.ts
@@ -7,8 +7,8 @@ const openPreview = (data) => {
 };
 const isLocal = process.env.NODE_ENV === 'development';
 const isDev = window.location.hostname.includes('preprod.local');
-const proxyUrl = isDev ? 'https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api'
-  : 'https://k9-los-oidc-auth-proxy.nais.adeo.no/api/k9-los-api';
+const proxyUrl = isDev ? 'https://k9-los-oidc-auth-proxy.nais.preprod.local/api/k9-los-api/'
+  : 'https://k9-los-oidc-auth-proxy.nais.adeo.no/api/k9-los-api/';
 
 const cancellable = (axiosInstance, config) => {
   let cancel;
