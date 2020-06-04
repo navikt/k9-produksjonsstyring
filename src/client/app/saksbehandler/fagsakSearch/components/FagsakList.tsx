334f7225ec7be4fc2d72b2ee5c814903f9ae7b99
alexandraban <alexandrabandarenka@nav.no>
2020-06-04 09:30:51 +0200

    fikser reservasjonsmodalen

--- src/client/app/saksbehandler/fagsakSearch/components/FagsakList.tsx
+++ src/client/app/saksbehandler/fagsakSearch/components/FagsakList.tsx
@@ -1,6 +1,5 @@
-
 import React, {
-  Component, Fragment, FunctionComponent, useEffect, useState,
+  Component, Fragment,
 } from 'react';
 import { connect } from 'react-redux';
 import NavFrontendChevron from 'nav-frontend-chevron';
@@ -34,71 +33,90 @@ interface OwnProps {
   fagsakOppgaver: Oppgave[];
 }
 
-let viseModal = false;
-
-const onClick = (oppgave, selectOppgaveCallback) => {
-  if (oppgave.erTilSaksbehandling && !oppgave.status.erReservert) {
-    viseModal = true;
-  } else {
-    selectOppgaveCallback(oppgave, false);
-  }
-};
-
-const onSubmit = (oppgave: Oppgave, selectOppgaveCallback) => {
-  viseModal = false;
-  selectOppgaveCallback(oppgave, true);
-};
-
-const onCancel = (oppgave: Oppgave, selectOppgaveCallback) => {
-  viseModal = false;
-  selectOppgaveCallback(oppgave, false);
-};
+interface OwnState {
+  visReserverOppgaveModal: boolean;
+}
 
 /**
  * FagsakList
  *
  * Presentasjonskomponent. Formaterer fagsak-søkeresultatet for visning i tabell. Sortering av fagsakene blir håndtert her.
  */
-export const FagsakList: FunctionComponent<OwnProps> = ({
-  sorterteFagsaker,
-  fagsakOppgaver,
-  selectOppgaveCallback,
-}) => (
-  <Table headerTextCodes={headerTextCodes} classNameTable={styles.table}>
-    {sorterteFagsaker.map((fagsak) => {
-      const filtrerteOppgaver = fagsakOppgaver.filter((o) => o.saksnummer === fagsak.saksnummer);
-      const oppgaver = filtrerteOppgaver.map((oppgave, index) => (
-        <TableRow
-          key={`oppgave${oppgave.eksternId}`}
-          id={oppgave.eksternId}
-          onMouseDown={() => onClick(oppgave, selectOppgaveCallback)}
-          onKeyDown={() => onClick(oppgave, selectOppgaveCallback)}
-          isDashedBottomBorder={filtrerteOppgaver.length > index + 1}
-        >
-          <TableColumn>{oppgave.saksnummer}</TableColumn>
-          <TableColumn>{oppgave.navn}</TableColumn>
-          <TableColumn>{oppgave.fagsakYtelseType.navn}</TableColumn>
-          <TableColumn>{oppgave.behandlingstype.navn}</TableColumn>
-          <TableColumn><NavFrontendChevron /></TableColumn>
-          {viseModal && (
-          <ReserverOppgaveModal
-            cancel={() => onCancel(oppgave, selectOppgaveCallback)}
-            valgtOppgave={oppgave}
-            submit={() => onSubmit(oppgave, selectOppgaveCallback)}
-            selectOppgaveCallback={() => selectOppgaveCallback}
-          />
-          )}
-        </TableRow>
-      ));
+export class FagsakList extends Component<OwnProps, OwnState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visReserverOppgaveModal: false,
+    };
+  }
+
+  onClick = (oppgave, selectOppgaveCallback) => {
+    if (oppgave.erTilSaksbehandling && !oppgave.status.erReservert) {
+      this.setState((prevState) => ({ ...prevState, visReserverOppgaveModal: true }));
+    } else {
+      selectOppgaveCallback(oppgave, false);
+    }
+  };
+
+  onSubmit = (oppgave: Oppgave, selectOppgaveCallback) => {
+    this.setState((prevState) => ({ ...prevState, visReserverOppgaveModal: false }));
+    selectOppgaveCallback(oppgave, true);
+  };
+
+  onCancel = (oppgave: Oppgave, selectOppgaveCallback) => {
+    this.setState((prevState) => ({ ...prevState, visReserverOppgaveModal: true }));
+    selectOppgaveCallback(oppgave, false);
+  };
+
+    render = () => {
+      const {
+        sorterteFagsaker,
+        fagsakOppgaver,
+        selectOppgaveCallback,
+      } = this.props;
+      const {
+        visReserverOppgaveModal,
+      } = this.state;
 
       return (
-        <Fragment key={`fagsak${fagsak.saksnummer}`}>
-          {oppgaver.length > 0 && oppgaver}
-        </Fragment>
+        <Table headerTextCodes={headerTextCodes} classNameTable={styles.table}>
+          {sorterteFagsaker.map((fagsak) => {
+            const filtrerteOppgaver = fagsakOppgaver.filter((o) => o.saksnummer === fagsak.saksnummer);
+            const oppgaver = filtrerteOppgaver.map((oppgave, index) => (
+              <TableRow
+                key={`oppgave${oppgave.eksternId}`}
+                id={oppgave.eksternId}
+                onMouseDown={() => this.onClick(oppgave, selectOppgaveCallback)}
+                onKeyDown={() => this.onClick(oppgave, selectOppgaveCallback)}
+                isDashedBottomBorder={filtrerteOppgaver.length > index + 1}
+              >
+                <TableColumn>{oppgave.saksnummer}</TableColumn>
+                <TableColumn>{oppgave.navn}</TableColumn>
+                <TableColumn>{oppgave.fagsakYtelseType.navn}</TableColumn>
+                <TableColumn>{oppgave.behandlingstype.navn}</TableColumn>
+                <TableColumn><NavFrontendChevron /></TableColumn>
+                {visReserverOppgaveModal && (
+                <ReserverOppgaveModal
+                  cancel={() => this.onCancel(oppgave, selectOppgaveCallback)}
+                  valgtOppgave={oppgave}
+                  submit={() => this.onSubmit(oppgave, selectOppgaveCallback)}
+                  selectOppgaveCallback={() => selectOppgaveCallback}
+                />
+                )}
+              </TableRow>
+            ));
+
+            return (
+              <Fragment key={`fagsak${fagsak.saksnummer}`}>
+                {oppgaver.length > 0 && oppgaver}
+              </Fragment>
+            );
+          })}
+        </Table>
       );
-    })}
-  </Table>
-);
+    }
+}
 
 const mapStateToProps = (state) => ({
   sorterteFagsaker: getFagsaker(state),
