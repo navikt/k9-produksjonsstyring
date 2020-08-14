1d8784b97201f766e46216956636914698602f72
alexandraban <alexandrabandarenka@nav.no>
2020-08-14 12:54:21 +0200

    endrer lenken til systemrutinene

--- src/client/app/api/eksterneLenker.tsx
+++ src/client/app/api/eksterneLenker.tsx
@@ -1,4 +1,4 @@
 export const RETTSKILDE_URL = 'https://lovdata.no/pro/sso/login/nav';
 
 // eslint-disable-next-line max-len
-export const SYSTEMRUTINE_URL = 'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/Saksbehandlingsl%C3%B8sningen-for-foreldrepenger-og-engangst%C3%B8nad.aspx';
+export const SYSTEMRUTINE_URL = 'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/K9-saksbehandlingsl%C3%B8sningen%20for%20pleiepenger,%20omsorgspenger,%20oppl%C3%A6ringspenger%20og%20pleie%20av%20n%C3%A6rst%C3%A5ende.aspx';
