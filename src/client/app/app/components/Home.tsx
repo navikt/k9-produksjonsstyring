2edbf94a1eba5765171cea1683670b69cf4d6e89
alexandraban <alexandrabandarenka@nav.no>
2021-01-07 15:27:52 +0100

    reverter

--- src/client/app/app/components/Home.tsx
+++ src/client/app/app/components/Home.tsx
@@ -9,7 +9,6 @@ import MissingPage from './MissingPage';
 import styles from './home.less';
 import AktoerIndex from '../../aktoer/AktoerIndex';
 
-export const aktoerPath = '/aktoer';
 interface OwnProps {
     headerHeight: number;
 }
@@ -27,7 +26,7 @@ const Home: FunctionComponent<OwnProps> = ({
       <Route exact path="/" component={SaksbehandlerIndex} />
       <Route exact path="/avdelingsleder" component={AvdelingslederIndex} />
       <Route exact path="/admin" component={AdminIndex} />
-      <Route strict path={aktoerPath} component={AktoerIndex} />
+      <Route exact path="/aktoer/:aktoerId" component={AktoerIndex} />
       <Route component={MissingPage} />
     </Switch>
   </div>
