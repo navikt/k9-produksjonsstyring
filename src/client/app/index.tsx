a77da78a73ea38aacfbd1e84a688e9592533ebb2
alexandraban <alexandrabandarenka@nav.no>
2021-02-24 09:39:36 +0100

    oppdaterer deps

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import createBrowserHistory from 'history';
 import { render } from 'react-dom';
 import { init } from '@sentry/browser';
 
