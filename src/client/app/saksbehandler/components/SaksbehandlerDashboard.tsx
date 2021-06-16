df123d344ea99f97437907108538b668a63d4783
eras-dev <emma.rasmusson@nav.no>
2021-06-16 10:51:50 +0200

    Fjernet url

--- src/client/app/saksbehandler/components/SaksbehandlerDashboard.tsx
+++ src/client/app/saksbehandler/components/SaksbehandlerDashboard.tsx
@@ -43,7 +43,6 @@ export const SaksbehandlerDashboard: FunctionComponent<OwnProps> = ({
                   omsorgspengerUrl={k9sakUrl.verdi}
                   k9sakUrl={k9sakUrl.verdi}
                   k9punsjUrl={k9punsjUrl.verdi}
-                  omsorgspengerUrl={omsorgspengerUrl.verdi}
                   setValgtOppgavekoId={setValgtOppgavekoId}
                   valgtOppgavekoId={valgtOppgavekoId}
                 />
