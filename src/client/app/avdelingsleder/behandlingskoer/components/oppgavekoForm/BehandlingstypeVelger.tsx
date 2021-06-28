5fa8833880649d795dbceb74bf9b8dd78e677482
eras-dev <emma.rasmusson@nav.no>
2021-06-28 13:30:21 +0200

    Lagt in samling for punsjoppgave

--- src/client/app/avdelingsleder/behandlingskoer/components/oppgavekoForm/BehandlingstypeVelger.tsx
+++ src/client/app/avdelingsleder/behandlingskoer/components/oppgavekoForm/BehandlingstypeVelger.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent, useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Normaltekst } from 'nav-frontend-typografi';
 
@@ -9,6 +9,8 @@ import { CheckboxField } from 'form/FinalFields';
 import useRestApiRunner from 'api/rest-api-hooks/src/local-data/useRestApiRunner';
 import { K9LosApiKeys } from 'api/k9LosApi';
 import useKodeverk from 'api/rest-api-hooks/src/global-data/useKodeverk';
+import { RenderCheckboxField } from 'form/finalFields/CheckboxField';
+import { Checkbox as NavCheckbox } from 'nav-frontend-skjema';
 import styles from './utvalgskriterierForOppgavekoForm.less';
 
 const behandlingstypeOrder = Object.values(behandlingType);
@@ -26,16 +28,31 @@ const BehandlingstypeVelger: FunctionComponent<OwnProps> = ({
   hentOppgaveko,
 }) => {
   const { startRequest: lagreOppgavekoBehandlingstype } = useRestApiRunner(K9LosApiKeys.LAGRE_OPPGAVEKO_BEHANDLINGSTYPE);
+  const punsjBehandlingstyper = [
+    'PAPIRSØKNAD',
+    'PAPIRETTERSENDELSE',
+    'PAPIRINNTEKTSOPPLYSNINGER',
+    'DIGITAL_ETTERSENDELSE',
+    'INNLOGGET_CHAT',
+    'SKRIV_TIL_OSS_SPØRMSÅL',
+    'SKRIV_TIL_OSS_SVAR',
+    'SAMTALEREFERAT',
+    'UKJENT'];
+
   const alleBehandlingTyper = useKodeverk(kodeverkTyper.BEHANDLING_TYPE);
-  const behandlingTyper = useMemo(() => behandlingstypeOrder.map((kode) => alleBehandlingTyper.find((bt) => bt.kode === kode)),
-    []);
+
+  const behandlingTyper = behandlingstypeOrder.map((kode) => alleBehandlingTyper.find((bt) => bt.kode === kode));
+  const behandlingTyperIkkePunsj = useMemo(() => behandlingTyper.filter((type) => !punsjBehandlingstyper.includes(type.kode)), []);
+  const behandlingTyperPunsj = useMemo(() => behandlingTyper.filter((type) => punsjBehandlingstyper.includes(type.kode)), []);
+  const [visPunsj, setVisPunsj] = useState<boolean>(false);
+
   return (
     <>
       <Normaltekst className={styles.label}>
         <FormattedMessage id="BehandlingstypeVelger.Behandlingstype" />
       </Normaltekst>
       <VerticalSpacer eightPx />
-      {behandlingTyper.map((bt) => (
+      {behandlingTyperIkkePunsj.map((bt) => (
         <React.Fragment key={bt.kode}>
           <VerticalSpacer fourPx />
           <CheckboxField
@@ -47,8 +64,36 @@ const BehandlingstypeVelger: FunctionComponent<OwnProps> = ({
           />
         </React.Fragment>
       ))}
+
+      <VerticalSpacer fourPx />
+      <NavCheckbox
+        className={styles.punsjOverskrift}
+        onChange={(isChecked) => setVisPunsj(isChecked.target.checked)}
+        checked={visPunsj}
+        label="Punsj"
+      />
+
+      {visPunsj && (
+        <div className={styles.punsjUndervalg}>
+          {behandlingTyperPunsj.map((bt) => (
+            <React.Fragment key={bt.kode}>
+              <VerticalSpacer fourPx />
+              <CheckboxField
+                name={bt.kode}
+                label={bt.navn}
+                onChange={(isChecked) => lagreOppgavekoBehandlingstype({
+                  id: valgtOppgavekoId,
+                  behandlingType: bt,
+                  checked: isChecked,
+                }).then(() => {
+                  hentOppgaveko(valgtOppgavekoId);
+                })}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+      )}
     </>
   );
 };
-
 export default BehandlingstypeVelger;
