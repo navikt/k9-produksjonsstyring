6519ad1e7baa2c0910e9116333d81e7015fe55ad
eras-dev <emma.rasmusson@nav.no>
2021-08-24 14:15:21 +0200

    Köper app men ikke tester -React 17

--- webpack/webpack.common.dev_and_prod.js
+++ webpack/webpack.common.dev_and_prod.js
@@ -99,10 +99,6 @@ const config = {
   },
 
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? 'style.css' : 'style_[hash].css',
-      ignoreOrder: true,
-    }),
     new CircularDependencyPlugin({
       exclude: /node_modules/,
       failOnError: true,
--- webpack/webpack.common.test.js
+++ webpack/webpack.common.test.js
@@ -28,7 +28,7 @@ const config = {
           },
         },
       ],
-    },],
+    }],
   },
 
   plugins: [],
