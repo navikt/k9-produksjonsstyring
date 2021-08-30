91094d8dd9c652af56807d9d94b1efbf76b8cbdb
eras-dev <emma.rasmusson@nav.no>
2021-08-30 09:51:25 +0200

    Lagt til kopi i test

--- src/client/app/avdelingsleder/behandlingskoer/components/oppgavekoForm/BehandlingstypeVelger.spec.tsx
+++ src/client/app/avdelingsleder/behandlingskoer/components/oppgavekoForm/BehandlingstypeVelger.spec.tsx
@@ -57,6 +57,9 @@ const behandlingTyper = [{
 {
   kode: behandlingType.SAMTALEREFERAT,
   navn: 'Samtalereferat',
+}, {
+  kode: behandlingType.KOPI,
+  navn: 'Kopi',
 },
 ];
 
@@ -72,9 +75,9 @@ describe('<BehandlingstypeVelger>', () => {
         />);
 
         const checkboxer = wrapper.find(CheckboxField);
-        expect(checkboxer).to.have.length(15);
+        expect(checkboxer).to.have.length(16);
         expect(checkboxer.first().prop('name')).to.eql(behandlingType.ANKE);
-        expect(checkboxer.last().prop('name')).to.eql(behandlingType.UKJENT);
+        expect(checkboxer.last().prop('name')).to.eql(behandlingType.KOPI);
       });
   });
 
