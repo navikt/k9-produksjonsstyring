b23e6309c015bed2863126fb50581bec21760e35
eras-dev <emma.rasmusson@nav.no>
2021-09-30 07:50:40 +0200

    lagt in navn for saksbehandler når en oppgave er reservert

--- src/client/app/saksbehandler/components/OppgaveErReservertAvAnnenModal.tsx
+++ src/client/app/saksbehandler/components/OppgaveErReservertAvAnnenModal.tsx
@@ -57,6 +57,7 @@ export const OppgaveErReservertAvAnnenModal: FunctionComponent<OwnProps & Wrappe
             id="OppgaveErReservertAvAnnenModal.ReservertAv"
             values={{
               saksbehandlerid: oppgaveStatus.reservertAv,
+              saksbehandlernavn: !!oppgaveStatus.reservertAvNavn ? oppgaveStatus.reservertAvNavn : '',
               ...getDateAndTime(oppgaveStatus.reservertTilTidspunkt),
             }}
           />
