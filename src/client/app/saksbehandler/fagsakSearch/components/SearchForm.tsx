2fdc86606a9f22090d4e6165023dcba95b680885
eras-dev <emma.rasmusson@nav.no>
2021-10-08 12:52:22 +0200

    fjerner ubrukt import

--- src/client/app/saksbehandler/fagsakSearch/components/SearchForm.tsx
+++ src/client/app/saksbehandler/fagsakSearch/components/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 import {
   injectIntl, WrappedComponentProps, FormattedMessage,
 } from 'react-intl';
