1c81627b64a4e464a37fe26bba720f3b2d95eff4
eras-dev <emma.rasmusson@nav.no>
2021-10-15 20:13:50 +0200

    oppdater tester

--- src/client/app/saksbehandler/behandlingskoer/components/OppgavekoPanel.spec.tsx
+++ src/client/app/saksbehandler/behandlingskoer/components/OppgavekoPanel.spec.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
-
 import OppgavekoVelgerForm from './OppgavekoVelgerForm';
-
 import OppgavekoPanel from './OppgavekoPanel';
 
 describe('<OppgavekoPanel>', () => {
   it('skal vise kriterievelger og liste over neste saker', () => {
-    const reserverteOppgaver = [];
-    const oppgavekoer = [];
     const wrapper = shallow(<OppgavekoPanel
-      oppgaverTilBehandling={reserverteOppgaver}
-      reserverteOppgaver={reserverteOppgaver}
-      oppgavekoer={oppgavekoer}
+      oppgaverTilBehandling={[]}
+      reserverteOppgaver={[]}
+      oppgavekoer={[]}
       reserverOppgave={sinon.spy()}
       setValgtOppgavekoId={sinon.spy()}
       valgtOppgavekoId="1"
@@ -24,5 +20,13 @@ describe('<OppgavekoPanel>', () => {
 
     expect(wrapper.find(OppgavekoVelgerForm)).to.have.length(1);
     expect(wrapper.find('button')).to.have.length(2);
+
+    expect(wrapper.find('.behandlingskoerContainer').at(0).children()).to.have.length(1);
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.find('.behandlingskoerContainer').at(0).children()).to.have.length(2);
+
+    expect(wrapper.find('.behandlingskoerContainer').at(1).children()).to.have.length(1);
+    wrapper.find('button').at(1).simulate('click');
+    expect(wrapper.find('.behandlingskoerContainer').at(1).children()).to.have.length(2);
   });
 });
