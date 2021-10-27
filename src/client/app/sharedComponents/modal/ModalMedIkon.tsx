f936f42a74ddbde4a08b062ae3b3f30826b4953f
eras-dev <emma.rasmusson@nav.no>
2021-10-27 10:22:30 +0200

    lagt in hantering av error och on loading mot apikall för att få oppgave

--- src/client/app/sharedComponents/modal/ModalMedIkon.tsx
+++ src/client/app/sharedComponents/modal/ModalMedIkon.tsx
@@ -12,11 +12,11 @@ import styles from './modalMedIkon.less';
 
 type TsProps = Readonly<{
   cancel: () => void;
-  submit: () => void;
+  submit?: () => void;
     tekst: {
       formattedMessageId: string;
       values?: Record<string, boolean | string | number>;
-      valgmulighetA: string;
+      valgmulighetA?: string;
       valgmulighetB: string;
     }
     ikonUrl: string;
@@ -47,6 +47,7 @@ const ModalMedIkon = ({
         </div>
       </div>
       <div className={classnames(styles.row, styles.buttons)}>
+        {tekst.valgmulighetA && typeof submit !== 'undefined' && (
         <Hovedknapp
           className={styles.submitButton}
           mini
@@ -56,6 +57,7 @@ const ModalMedIkon = ({
         >
           {tekst.valgmulighetA}
         </Hovedknapp>
+        )}
         <Knapp
           className={styles.cancelButton}
           mini
