544d71634bfd36656a9439ac6ff7dc5ee21e23c0
eras-dev <emma.rasmusson@nav.no>
2021-10-28 20:17:54 +0200

    tester ny setup for svgs

--- webpack/webpack.common.dev_and_prod.js
+++ webpack/webpack.common.dev_and_prod.js
@@ -1,14 +1,13 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-const { merge } = require('webpack-merge');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const common = require('./webpack.common.js');
 
 const CORE_DIR = path.resolve(__dirname, '../node_modules');
 const ROOT_DIR = path.resolve(__dirname, '../src/client');
 const APP_DIR = path.join(ROOT_DIR, 'app');
 const STYLE_DIR = path.join(ROOT_DIR, 'styles');
+const IMG_DIR = path.join(ROOT_DIR, 'images');
 
 const isDevelopment = JSON.stringify(process.env.NODE_ENV) === '"development"';
 
@@ -76,10 +75,12 @@ const config = {
           },
         ],
         include: [STYLE_DIR, CORE_DIR],
-      }, {
-        test: /\.svg/,
+      },
+      {
+        test: /\.(svg)$/,
         type: 'asset/resource',
-      }
+        include: [IMG_DIR],
+      },
     ],
   },
 
@@ -96,6 +97,32 @@ const config = {
       failOnError: true,
     }),
   ],
+
+  resolve: {
+    alias: {
+      styles: path.join(ROOT_DIR, 'styles'),
+      images: path.join(ROOT_DIR, 'images'),
+      testHelpers: path.join(ROOT_DIR, 'testHelpers'),
+      app: path.join(APP_DIR, 'app'),
+      navAnsatt: path.join(APP_DIR, 'navAnsatt'),
+      form: path.join(APP_DIR, 'form'),
+      saksbehandler: path.join(APP_DIR, 'saksbehandler'),
+      avdelingsleder: path.join(APP_DIR, 'avdelingsleder'),
+      aktoer: path.join(APP_DIR, 'aktoer'),
+      api: path.join(APP_DIR, 'api'),
+      kodeverk: path.join(APP_DIR, 'kodeverk'),
+      sharedComponents: path.join(APP_DIR, 'sharedComponents'),
+      utils: path.join(APP_DIR, 'utils'),
+    },
+    extensions: ['.json', '.js', '.tsx', '.ts', '.less'],
+  },
+
+  externals: {
+    cheerio: 'window',
+    'react/addons': 'react',
+    'react/lib/ExecutionEnvironment': 'react',
+    'react/lib/ReactContext': 'react',
+  },
 };
 
-module.exports = merge(common, config);
+module.exports = config;
