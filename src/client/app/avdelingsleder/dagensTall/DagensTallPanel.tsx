6b02621e8aafe6d8357a6b5bdd5c653af12d8bd9
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-11-01 09:41:37 +0100

    Slå sammen punsj i dagens tall (#580)
    * slog samma alla behandlingstyper i dagens tall
    
    * la in punsjTall i array istället for punsj behandlinger
    
    * lagt in behandlingstype i objeektet for punsj tall
    
    * lagt in samtalereferat med
    
    * tester att bruke PUNSJ_INNSENDING_TYPE istället for array med behandlinugstyper

--- src/client/app/avdelingsleder/dagensTall/DagensTallPanel.tsx
+++ src/client/app/avdelingsleder/dagensTall/DagensTallPanel.tsx
@@ -21,12 +21,23 @@ const DagensTallPanel: FunctionComponent<OwnProps & WrappedComponentProps> = ({ 
   const behandlingsKoderSomSkalVisesForst = [behandlingType.ANKE, behandlingType.FORSTEGANGSSOKNAD, behandlingType.INNSYN,
     behandlingType.KLAGE, behandlingType.REVURDERING, behandlingType.TILBAKEBETALING];
 
+  const punsjBehandlinger = [];
   sortedDagensTall(dagensTall).forEach((dt) => {
     if (behandlingsKoderSomSkalVisesForst.includes(dt.behandlingType.kode)) behandlingstyperForst.push(dt);
+    else if (dt.behandlingType.kodeverk && dt.behandlingType.kodeverk === 'PUNSJ_INNSENDING_TYPE') punsjBehandlinger.push(dt);
     else behandlingstyperSist.push(dt);
   });
 
-  const dagensTallIRettRekkefoljd = behandlingstyperForst.concat(behandlingstyperSist);
+  const punsjTall = {
+    antall: 0,
+    behandlingType: {
+      navn: 'Punsj',
+    },
+  };
+
+  punsjBehandlinger.forEach((behandlingstype) => { punsjTall.antall += behandlingstype.antall; });
+
+  const dagensTallIRettRekkefoljd = [...behandlingstyperForst, ...behandlingstyperSist, punsjTall];
 
   return (
     <div className={styles.dagensTallContainer}>
