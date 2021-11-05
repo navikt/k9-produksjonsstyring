e395d78c09b267ea82263316bdfcc2e4f0825d37
eras-dev <emma.rasmusson@nav.no>
2021-11-05 10:06:55 +0100

    fixat code smell og indentering

--- src/client/mocks/ferdigstilteHistorikk.ts
+++ src/client/mocks/ferdigstilteHistorikk.ts
@@ -1,4 +1,4 @@
-const ferdigstilteHistorikk = [ {
+const ferdigstilteHistorikk = [{
   fagsakYtelseType: {
     kode: 'OMP',
     navn: 'Omsorgspenger',
@@ -12,272 +12,271 @@ const ferdigstilteHistorikk = [ {
   dato: '2021-08-29',
   antall: 1,
 },
-  {
-    fagsakYtelseType: {
-      kode: 'OMP',
-      navn: 'Omsorgspenger',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-007',
-      navn: 'Tilbakekreving',
-      kodeverk: 'ae0203',
-    },
-    dato: '2021-08-30',
-    antall: 1,
+{
+  fagsakYtelseType: {
+    kode: 'OMP',
+    navn: 'Omsorgspenger',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'OMP',
-      navn: 'Omsorgspenger',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-004',
-      navn: 'Revurdering',
-      kodeverk: 'ae0028',
-    },
-    dato: '2021-08-29',
-    antall: 2,
+  behandlingType: {
+    kode: 'BT-007',
+    navn: 'Tilbakekreving',
+    kodeverk: 'ae0203',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'OMP_MA',
-      navn: 'Omsorgsdager: midlertidig alene',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-004',
-      navn: 'Revurdering',
-      kodeverk: 'ae0028',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-30',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'OMP',
+    navn: 'Omsorgspenger',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'OMP_KS',
-      navn: 'Omsorgsdager: midlertidig alene',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-002',
-      navn: 'Førstegangsbehandling',
-      kodeverk: 'ae0028',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-004',
+    navn: 'Revurdering',
+    kodeverk: 'ae0028',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-002',
-      navn: 'Førstegangsbehandling',
-      kodeverk: 'ae0034',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 2,
+},
+{
+  fagsakYtelseType: {
+    kode: 'OMP_MA',
+    navn: 'Omsorgsdager: midlertidig alene',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-007',
-      navn: 'Tilbakekreving',
-      kodeverk: 'ae0203',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-004',
+    navn: 'Revurdering',
+    kodeverk: 'ae0028',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'BT-004',
-      navn: 'Revurdering',
-      kodeverk: 'ae0028',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'OMP_KS',
+    navn: 'Omsorgsdager: midlertidig alene',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'PAPIRINNTEKTSOPPLYSNINGER',
-      navn: 'Papirinntektsopplysninger',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-002',
+    navn: 'Førstegangsbehandling',
+    kodeverk: 'ae0028',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'KOPI',
-      navn: 'Kopi',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'DIGITAL_ETTERSENDELSE',
-      navn: 'Digital ettersendelse',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-002',
+    navn: 'Førstegangsbehandling',
+    kodeverk: 'ae0034',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'SAMTALEREFERAT',
-      navn: 'Samtalereferat',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'SAMTALEREFERAT',
-      navn: 'Samtalereferat',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-007',
+    navn: 'Tilbakekreving',
+    kodeverk: 'ae0203',
   },
-  ,
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'SAMTALEREFERAT',
-      navn: 'Samtalereferat',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'SKRIV_TIL_OSS_SPØRMSÅL',
-      navn: 'Skriv til oss spørmsål',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'BT-004',
+    navn: 'Revurdering',
+    kodeverk: 'ae0028',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'SKRIV_TIL_OSS_SVAR',
-      navn: 'Skriv til oss svar',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'PAPIRSØKNAD',
-      navn: 'Papirsøknad',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'PAPIRINNTEKTSOPPLYSNINGER',
+    navn: 'Papirinntektsopplysninger',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'PAPIRETTERSENDELSE',
-      navn: 'Papirettersendelse',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'INNLOGGET_CHAT',
-      navn: 'Innlogget chat',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-29',
-    antall: 1,
+  behandlingType: {
+    kode: 'KOPI',
+    navn: 'Kopi',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
   },
-  {
-    fagsakYtelseType: {
-      kode: 'PSB',
-      navn: 'Pleiepenger sykt barn',
-      kodeverk: 'FAGSAK_YTELSE_TYPE',
-    },
-    behandlingType: {
-      kode: 'UKJENT',
-      navn: 'Ukjent',
-      kodeverk: 'PUNSJ_INNSENDING_TYPE',
-    },
-    dato: '2021-08-30',
-    antall: 1,
-  }];
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'DIGITAL_ETTERSENDELSE',
+    navn: 'Digital ettersendelse',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'SAMTALEREFERAT',
+    navn: 'Samtalereferat',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'SAMTALEREFERAT',
+    navn: 'Samtalereferat',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'SAMTALEREFERAT',
+    navn: 'Samtalereferat',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'SKRIV_TIL_OSS_SPØRMSÅL',
+    navn: 'Skriv til oss spørmsål',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'SKRIV_TIL_OSS_SVAR',
+    navn: 'Skriv til oss svar',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'PAPIRSØKNAD',
+    navn: 'Papirsøknad',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'PAPIRETTERSENDELSE',
+    navn: 'Papirettersendelse',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'INNLOGGET_CHAT',
+    navn: 'Innlogget chat',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-29',
+  antall: 1,
+},
+{
+  fagsakYtelseType: {
+    kode: 'PSB',
+    navn: 'Pleiepenger sykt barn',
+    kodeverk: 'FAGSAK_YTELSE_TYPE',
+  },
+  behandlingType: {
+    kode: 'UKJENT',
+    navn: 'Ukjent',
+    kodeverk: 'PUNSJ_INNSENDING_TYPE',
+  },
+  dato: '2021-08-30',
+  antall: 1,
+}];
 
 export default ferdigstilteHistorikk;
