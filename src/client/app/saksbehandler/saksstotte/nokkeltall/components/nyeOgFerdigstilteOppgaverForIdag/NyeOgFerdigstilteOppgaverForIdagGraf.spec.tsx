7688e311bc6ba0ff4b694800dc2a4d3ed47f9765
eras-dev <emma.rasmusson@nav.no>
2021-11-18 10:19:50 +0100

    fixat test

--- src/client/app/saksbehandler/saksstotte/nokkeltall/components/nyeOgFerdigstilteOppgaverForIdag/NyeOgFerdigstilteOppgaverForIdagGraf.spec.tsx
+++ src/client/app/saksbehandler/saksstotte/nokkeltall/components/nyeOgFerdigstilteOppgaverForIdag/NyeOgFerdigstilteOppgaverForIdagGraf.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { expect } from 'chai';
 import { IntlShape } from 'react-intl';
 import ReactECharts from 'sharedComponents/echart/ReactEcharts';
+import behandlingType from 'kodeverk/behandlingType';
+import moment from 'moment';
 import { shallowWithIntl, intlMock } from '../../../../../../../../setup/testHelpers/intl-enzyme-test-helper';
 
 import NyeOgFerdigstilteOppgaverForIdagGraf from './NyeOgFerdigstilteOppgaverForIdagGraf';
@@ -10,6 +12,21 @@ describe('<NyeOgFerdigstilteOppgaverForIdagGraf>', () => {
   const intl: Partial<IntlShape> = {
     ...intlMock,
   };
+
+  const nyeOgFerdigstilteOppgaver = [{
+    behandlingType: {
+      kode: behandlingType.FORSTEGANGSSOKNAD,
+      navn: 'Navn',
+    },
+    fagsakYtelseType: {
+      kode: 'PSB',
+      navn: 'Pleiepenger',
+    },
+    antallNye: 1,
+    antallFerdigstilte: 6,
+    antallFerdigstilteMine: 1,
+    dato: moment().subtract(3, 'days').format(),
+  }];
 
   it('skal vise ReactEchartGraf', () => {
     const wrapper = shallowWithIntl(<NyeOgFerdigstilteOppgaverForIdagGraf.WrappedComponent
