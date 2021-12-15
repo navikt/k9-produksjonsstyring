9a394e9ff038407448638ddac80892ccda2cd315
eras-dev <emma.rasmusson@nav.no>
2021-12-15 09:08:05 +0100

    fjerne variabel som ikkeb rukes

--- src/client/app/saksbehandler/fagsakSearch/components/FagsakSearch.spec.tsx
+++ src/client/app/saksbehandler/fagsakSearch/components/FagsakSearch.spec.tsx
@@ -9,37 +9,6 @@ import SearchForm from './SearchForm';
 import FagsakList from './FagsakList';
 
 describe('<FagsakSearch>', () => {
-  const oppgave = {
-    status: {
-      erReservert: false,
-      reservertTilTidspunkt: null,
-      erReservertAvInnloggetBruker: false,
-      reservertAv: null,
-      flyttetReservasjon: null,
-    },
-    saksnummer: '23456',
-    behandlingId: 234,
-    personnummer: '23089076787',
-    navn: 'Walter',
-    system: 'VL',
-    behandlingstype: {
-      navn: 'Førstegangssøknad',
-      kode: 'BT-002',
-    },
-    behandlingStatus: {
-      navn: 'Opprettet',
-      kode: 'OPPRE',
-    },
-    opprettetTidspunkt: '',
-    behandlingsfrist: '',
-    fagsakYtelseType: {
-      navn: 'Omsorgspenger',
-      kode: 'OMS',
-    },
-    erTilSaksbehandling: true,
-    eksternId: '',
-  };
-
   it('skal kun vise søkefelt før søk er startet', () => {
     const searchFagsakFunction = sinon.spy();
     const wrapper = shallow(<FagsakSearch
