14fbdfa1d1b729d061977925fbe85ace7fae164c
vebnor <vebjorn.nordby@nav.no>
2022-02-12 13:54:26 +0100

    er dette feature toggling

--- scripts/envSettings.js
+++ scripts/envSettings.js
@@ -1,11 +1,12 @@
 const fsExtra = require('fs-extra');
 
 function createEnvSettingsFile(settingsFile) {
-  fsExtra.ensureFile(settingsFile).then((f) => {
+  fsExtra.ensureFile(settingsFile).then(f => {
     fsExtra.writeFileSync(
       settingsFile,
       `window.appSettings = {OIDC_AUTH_PROXY: '${process.env.OIDC_AUTH_PROXY}', REDIRECT_URL: '${process.env.REDIRECT_URL}', 
-      K9_PUNSJ_URL: '${process.env.K9_PUNSJ_URL}'};`,
+      K9_PUNSJ_URL: '${process.env.K9_PUNSJ_URL}'};,
+      FEATURE_TOGGLES: '${process.env.FEATURE_TOGGLES}`,
     );
   });
 }
