55bbd987aebd46311705a023653ddf6e3cce9dd7
vebnor <vebjorn.nordby@nav.no>
2022-02-17 15:10:41 +0100

    fikser tester og legger til alle providers som skal trenges

--- setup/testHelpers/testUtils.js
+++ setup/testHelpers/testUtils.js
@@ -3,7 +3,6 @@ import { render as rtlRender } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import { RestApiStateContext } from 'api/rest-api-hooks';
 import { RestApiProvider } from 'api/rest-api-hooks/src/RestApiContext';
 import { config } from 'utils/reactQueryConfig';
 import { k9LosApi } from 'api/k9LosApi';
@@ -24,15 +23,13 @@ export function renderWithIntl(ui, { locale, messages, ...renderOptions } = {}) 
 export const renderWithAllProviders = (ui, { ...renderOptions } = {}) => {
   const queryClient = new QueryClient(config);
   const Wrapper = ({ children }) => (
-    <RestApiStateContext.Provider value={{ KODEVERK: kodeverk }}>
-      <RestApiProvider requestApi={k9LosApi}>
-        <QueryClientProvider client={queryClient}>
-          <IntlProvider locale={'nb-NO'} messages={defaultMessages}>
-            {children}
-          </IntlProvider>
-        </QueryClientProvider>
-      </RestApiProvider>
-    </RestApiStateContext.Provider>
+    <RestApiProvider requestApi={k9LosApi} initialState={{ KODEVERK: kodeverk }}>
+      <QueryClientProvider client={queryClient}>
+        <IntlProvider locale={'nb-NO'} messages={defaultMessages}>
+          {children}
+        </IntlProvider>
+      </QueryClientProvider>
+    </RestApiProvider>
   );
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 };
