db3d4df2af0cba356f71ca649bcb3a072f0e3a5d
vebnor <vebjorn.nordby@nav.no>
2022-02-17 14:54:31 +0100

    fikser småpirk

--- setup/testHelpers/testUtils.js
+++ setup/testHelpers/testUtils.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { RestApiStateContext } from 'api/rest-api-hooks';
+import { RestApiProvider } from 'api/rest-api-hooks/src/RestApiContext';
+import { config } from 'utils/reactQueryConfig';
+import { k9LosApi } from 'api/k9LosApi';
 
 import defaultMessages from '../../src/client/app/sprak/nb_NO.json';
 import kodeverk from 'mocks/kodeverk';
@@ -17,11 +22,16 @@ export function renderWithIntl(ui, { locale, messages, ...renderOptions } = {}) 
 }
 
 export const renderWithAllProviders = (ui, { ...renderOptions } = {}) => {
+  const queryClient = new QueryClient(config);
   const Wrapper = ({ children }) => (
     <RestApiStateContext.Provider value={{ KODEVERK: kodeverk }}>
-      <IntlProvider locale={'nb-NO'} messages={defaultMessages}>
-        {children}
-      </IntlProvider>
+      <RestApiProvider requestApi={k9LosApi}>
+        <QueryClientProvider client={queryClient}>
+          <IntlProvider locale={'nb-NO'} messages={defaultMessages}>
+            {children}
+          </IntlProvider>
+        </QueryClientProvider>
+      </RestApiProvider>
     </RestApiStateContext.Provider>
   );
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
