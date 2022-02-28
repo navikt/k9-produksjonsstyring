ac5b4232ffd583e73d7a9e39973c1112b3b31eba
eras-dev <emma.rasmusson@nav.no>
2022-02-28 14:37:15 +0100

    testar legge till ErrorBoundry

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import { init } from '@sentry/browser';
+import { ErrorBoundary } from '@sentry/react';
+
 
 import AppIndex from 'app/AppIndex';
 import { RestApiProvider } from 'api/rest-api-hooks/src/RestApiContext';
@@ -28,15 +30,17 @@ const renderFunc = Component => {
   const queryClient = new QueryClient(config);
 
   render(
-    <BrowserRouter>
-      <RestApiProvider requestApi={k9LosApi}>
-        <RestApiErrorProvider>
-          <QueryClientProvider client={queryClient}>
-            <Component />
-          </QueryClientProvider>
-        </RestApiErrorProvider>
-      </RestApiProvider>
-    </BrowserRouter>,
+    <ErrorBoundary>
+      <BrowserRouter>
+        <RestApiProvider requestApi={k9LosApi}>
+          <RestApiErrorProvider>
+            <QueryClientProvider client={queryClient}>
+              <Component />
+            </QueryClientProvider>
+          </RestApiErrorProvider>
+        </RestApiProvider>
+      </BrowserRouter>
+    </ErrorBoundary>,
     app,
   );
 };
