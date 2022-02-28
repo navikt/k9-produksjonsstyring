ad95e8f38240cc2bca024b889ac4bd1de82b375e
eras-dev <emma.rasmusson@nav.no>
2022-02-28 11:37:29 +0100

    nytt försök

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+require('dotenv').config();
 
 const server = express();
 
-const isDev = window.location.hostname.includes('dev.adeo.no');
-const AUTH_PROXY_URL = isDev ? 'https://k9-los-oidc-auth-proxy.dev.intern.nav.no/api/k9-los-api'
-  : 'https://k9-los-oidc-auth-proxy.intern.nav.no/api/k9-los-api';
-
-// @ts-ignore
-console.log('AUTH_PROXY_URL', process.env.AUTH_PROXY_URL);
+console.log('AUTH_PROXY_URL', process.env.AUTH_PROXY_URL)
 
 server.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        connectSrc: ["'self'", AUTH_PROXY_URL, 'https://sentry.gc.nav.no'],
-        frameSrc: ["'self'", AUTH_PROXY_URL],
+        connectSrc: ["'self'", process.env.AUTH_PROXY_URL, 'https://sentry.gc.nav.no'],
+        frameSrc: ["'self'", process.env.AUTH_PROXY_URL],
         fontSrc: ["'self'", 'data:'],
         imgSrc: ["'self'", 'data:'],
       },
