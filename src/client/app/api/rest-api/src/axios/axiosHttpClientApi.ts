0e97dd28976db8ff7c84e9a27800a5eda4366086
eras-dev <emma.rasmusson@nav.no>
2022-03-14 15:09:31 +0100

    fixar så att interceptor också fungerar lokalt

--- src/client/app/api/rest-api/src/axios/axiosHttpClientApi.ts
+++ src/client/app/api/rest-api/src/axios/axiosHttpClientApi.ts
@@ -42,7 +42,10 @@ const getAxiosHttpClientApi = () => {
 
   // TODO Temp kode til backend returnerer string i staden for Kodeverk
   axiosInstance.interceptors.response.use((response: AxiosResponse): any => {
-    if (response.status === 200 && response.config.url && response.config.url.includes('/api/') && !response.config.url.includes('/k9-los-api/kodeverk')) {
+    if (response.status === 200 && response.config.url
+      && response.config.url.includes('/api/')
+      && !response.config.url.includes('/k9-los-api/kodeverk')
+      && !response.config.url.includes('/api/kodeverk')) {
       konverterKodeverkTilKode(response.data);
     }
     return response;
