b62eee1ed1303bc344eda05d008f7935f78b3839
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-10-11 14:49:09 +0200

    TSF-2182 - Deler ord i to (#1026)

--- src/client/app/avdelingsleder/dagensTall/DagensTallPanel.tsx
+++ src/client/app/avdelingsleder/dagensTall/DagensTallPanel.tsx
@@ -5,34 +5,50 @@ import { injectIntl, WrappedComponentProps } from 'react-intl';
 import ApneBehandlinger from 'avdelingsleder/dagensTall/apneBehandlingerTsType';
 import { behandlingstypeOrder, punsjKodeverkNavn } from 'avdelingsleder/nokkeltall/nokkeltallUtils';
 import behandlingType from 'kodeverk/behandlingType';
-import AlleKodeverk from "kodeverk/alleKodeverkTsType";
-import { useGlobalStateRestApiData } from "api/rest-api-hooks";
-import { RestApiGlobalStatePathsKeys } from "api/k9LosApi";
-import { getKodeverkFraKode, getKodeverknavnFraKode } from "utils/kodeverkUtils";
-import kodeverkTyper from "kodeverk/kodeverkTyper";
+import AlleKodeverk from 'kodeverk/alleKodeverkTsType';
+import { useGlobalStateRestApiData } from 'api/rest-api-hooks';
+import { RestApiGlobalStatePathsKeys } from 'api/k9LosApi';
+import { getKodeverkFraKode, getKodeverknavnFraKode } from 'utils/kodeverkUtils';
+import kodeverkTyper from 'kodeverk/kodeverkTyper';
 import styles from './dagensTallPanel.less';
 
 interface OwnProps {
-    totaltIdag: number;
-    dagensTall: ApneBehandlinger[];
+  totaltIdag: number;
+  dagensTall: ApneBehandlinger[];
 }
 
 const DagensTallPanel: FunctionComponent<OwnProps & WrappedComponentProps> = ({ totaltIdag, dagensTall }) => {
-  const sortedDagensTall = (tall: ApneBehandlinger[]) => useMemo(() => tall
-    .sort((dt1, dt2) => behandlingstypeOrder.indexOf(dt1.behandlingType) - behandlingstypeOrder.indexOf(dt2.behandlingType)), [dagensTall]);
+  const sortedDagensTall = (tall: ApneBehandlinger[]) =>
+    useMemo(
+      () =>
+        tall.sort(
+          (dt1, dt2) =>
+            behandlingstypeOrder.indexOf(dt1.behandlingType) - behandlingstypeOrder.indexOf(dt2.behandlingType),
+        ),
+      [dagensTall],
+    );
 
   const behandlingstyperSist = [];
   const behandlingstyperForst = [];
-  const behandlingsKoderSomSkalVisesForst = [behandlingType.ANKE, behandlingType.FORSTEGANGSSOKNAD, behandlingType.INNSYN,
-    behandlingType.KLAGE, behandlingType.REVURDERING, behandlingType.TILBAKEBETALING];
+  const behandlingsKoderSomSkalVisesForst = [
+    behandlingType.ANKE,
+    behandlingType.FORSTEGANGSSOKNAD,
+    behandlingType.INNSYN,
+    behandlingType.KLAGE,
+    behandlingType.REVURDERING,
+    behandlingType.TILBAKEBETALING,
+  ];
 
   const alleKodeverk: AlleKodeverk = useGlobalStateRestApiData(RestApiGlobalStatePathsKeys.KODEVERK);
 
   const punsjBehandlinger = [];
-  sortedDagensTall(dagensTall).forEach((dt) => {
+  sortedDagensTall(dagensTall).forEach(dt => {
     if (behandlingsKoderSomSkalVisesForst.includes(dt.behandlingType)) behandlingstyperForst.push(dt);
-    else if (dt.behandlingType
-      && getKodeverkFraKode(dt.behandlingType, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk) === punsjKodeverkNavn) punsjBehandlinger.push(dt);
+    else if (
+      dt.behandlingType &&
+      getKodeverkFraKode(dt.behandlingType, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk) === punsjKodeverkNavn
+    )
+      punsjBehandlinger.push(dt);
     else behandlingstyperSist.push(dt);
   });
 
@@ -41,9 +57,20 @@ const DagensTallPanel: FunctionComponent<OwnProps & WrappedComponentProps> = ({ 
     behandlingType: 'Punsj',
   };
 
-  punsjBehandlinger.forEach((behandlingstype) => { punsjTall.antall += behandlingstype.antall; });
+  punsjBehandlinger.forEach(behandlingstype => {
+    punsjTall.antall += behandlingstype.antall;
+  });
 
   const dagensTallIRettRekkefoljd = [...behandlingstyperForst, ...behandlingstyperSist, punsjTall];
+
+  const getTekst = (behandlingTypeFraDagensTall: string) => {
+    const tekst = getKodeverknavnFraKode(behandlingTypeFraDagensTall, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk);
+    if (tekst === 'Førstegangsbehandling') {
+      return `Førstegangs-\
+      behandling`;
+    }
+    return tekst;
+  };
 
   return (
     <div className={styles.dagensTallContainer}>
@@ -51,15 +78,20 @@ const DagensTallPanel: FunctionComponent<OwnProps & WrappedComponentProps> = ({ 
       <div className={styles.container}>
         <EnkelTeller antall={totaltIdag} tekst="Åpne behandlinger" />
 
-        {dagensTall && dagensTallIRettRekkefoljd.map((dt) => (
-          <EnkelTeller
-            key={dt.behandlingType === 'Punsj' ? 'Punsj' : getKodeverknavnFraKode(dt.behandlingType, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk)}
-            antall={dt.antall}
-            tekst={dt.behandlingType === 'Punsj' ? 'Punsj' : getKodeverknavnFraKode(dt.behandlingType, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk)} />
-        ))}
+        {dagensTall &&
+          dagensTallIRettRekkefoljd.map(dt => (
+            <EnkelTeller
+              key={
+                dt.behandlingType === 'Punsj'
+                  ? 'Punsj'
+                  : getKodeverknavnFraKode(dt.behandlingType, kodeverkTyper.BEHANDLING_TYPE, alleKodeverk)
+              }
+              antall={dt.antall}
+              tekst={dt.behandlingType === 'Punsj' ? 'Punsj' : getTekst(dt.behandlingType)}
+            />
+          ))}
       </div>
     </div>
-
   );
 };
 
