f78b3621628617e72b5e5bd962069ab9498b3891
vebnor <vebjorn.nordby@nav.no>
2023-03-11 10:23:31 +0100

    disabler tailwind preflight: preflight.css i tailwind har selectors med høy specificity som overskriver styles i 'nav-frontend-*-styles'-pakker og fjerner en del default css-attributter (f.eks default margins) som kan gjøre visningen rar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   presets: [require('@navikt/ds-tailwind')],
   content: ['./src/**/*.{js,jsx,ts,tsx}', '/dist/index.html'],
+  corePlugins: {
+    // https://tailwindcss.com/docs/preflight
+    // denne er disablet fordi:
+    // 1. preflight.css i tailwind har selectors med høy specificity som overskriver styles i "nav-frontend-*-styles"-pakker
+    // 2. den fjerner en del default css-attributter (f.eks default margins) som kan gjøre visningen rar
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
