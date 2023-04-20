b7e013aa94804aa5b7df4f3f29a7f5f1e5ed04dc
vebnor <vebjorn.nordby@nav.no>
2023-04-20 09:41:52 +0200

    prøver å feature toggle igjen

--- webpack/commonEnvConfig.js
+++ webpack/commonEnvConfig.js
@@ -1,6 +1,6 @@
 const commonEnvConfig = {
-	SAKSBEHANDLER_KAN_VELGE_NYE_KOER: 'false',
-	AVDELINGSLEDER_TILGANG_TIL_NYE_KOER: 'false',
+	SAKSBEHANDLER_KAN_VELGE_NYE_KOER: 'off',
+	AVDELINGSLEDER_TILGANG_TIL_NYE_KOER: 'off',
 };
 
 module.exports = commonEnvConfig;
