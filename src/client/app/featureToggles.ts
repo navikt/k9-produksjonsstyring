1a6daebddff449498fcd49040646bd3e8738e153
vebnor <vebjorn.nordby@nav.no>
2023-04-20 10:36:43 +0200

    prøver å feature toggle igjen

--- src/client/app/featureToggles.ts
+++ src/client/app/featureToggles.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 
-export const saksbehandlerKanVelgeNyeKoer = () => process.env.SAKSBEHANDLER_KAN_VELGE_NYE_KOER === 'on';
-export const avdelingslederTilgangTilNyeKoer = () => process.env.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER === 'on';
+export const saksbehandlerKanVelgeNyeKoer = () => process.env.SAKSBEHANDLER_KAN_VELGE_NYE_KOER === 'paa';
+export const avdelingslederTilgangTilNyeKoer = () => process.env.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER === 'paa';
--- webpack/commonEnvConfig.js
+++ webpack/commonEnvConfig.js
@@ -1,6 +1,6 @@
 const commonEnvConfig = {
-	SAKSBEHANDLER_KAN_VELGE_NYE_KOER: 'off',
-	AVDELINGSLEDER_TILGANG_TIL_NYE_KOER: 'off',
+	SAKSBEHANDLER_KAN_VELGE_NYE_KOER: 'av',
+	AVDELINGSLEDER_TILGANG_TIL_NYE_KOER: 'av',
 };
 
 module.exports = commonEnvConfig;
