8b7f391d9c0e8cdacb380f9a318ed50c37d7af42
vebnor <vebjorn.nordby@nav.no>
2023-04-28 20:08:45 +0200

    :)

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
 	retries: 0,
-	workers: 3,
+	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
