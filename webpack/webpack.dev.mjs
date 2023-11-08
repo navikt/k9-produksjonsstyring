be61fd4d28f605a4aea2ca25cf61ef07b00297ff
vebnor <vebjorn.nordby@nav.no>
2023-11-08 11:13:13 +0100

    forsøk på path fix til windows

--- webpack/webpack.dev.mjs
+++ webpack/webpack.dev.mjs
@@ -1,56 +1,53 @@
-import commonEnvConfig from "./commonEnvConfig.mjs";
-import commonDevAndProd from "./webpack.common.mjs";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
-import webpack from "webpack";
-import { merge } from "webpack-merge";
+import commonEnvConfig from './commonEnvConfig.mjs';
+import commonDevAndProd from './webpack.common.mjs';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import { merge } from 'webpack-merge';
 
-const dirname = path.dirname(new URL(import.meta.url).pathname);
-const ROOT_DIR = path.resolve(dirname, "../src/client");
-const APP_DIR = path.resolve(ROOT_DIR, "app");
+// windows path fix
+const dirname = decodeURIComponent(new URL(import.meta.url).pathname).replace(/^\/([A-Za-z]:\/)/, '$1');
+const ROOT_DIR = path.resolve(dirname, '../src/client');
+const APP_DIR = path.resolve(ROOT_DIR, 'app');
 
 const config = {
-  mode: "development",
-  devtool: "eval-cheap-module-source-map",
+	mode: 'development',
+	devtool: 'eval-cheap-module-source-map',
 
-  entry: [
-    "webpack-dev-server/client?http://localhost:8030",
-    "webpack/hot/only-dev-server",
-    `${APP_DIR}/index.tsx`,
-  ],
+	entry: ['webpack-dev-server/client?http://localhost:8030', 'webpack/hot/only-dev-server', `${APP_DIR}/index.tsx`],
 
-  output: {
-    globalObject: "this",
-    filename: "[name].js",
-    path: ROOT_DIR,
-    publicPath: "/",
-  },
+	output: {
+		globalObject: 'this',
+		filename: '[name].js',
+		path: ROOT_DIR,
+		publicPath: '/',
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      favicon: path.join(ROOT_DIR, "favicon.ico"),
-      template: path.join(ROOT_DIR, "index.html"),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.EnvironmentPlugin({
-      ...commonEnvConfig,
-      MSW_MODE: process.env.MSW_MODE || "disabled",
-      SENTRY_RELEASE: "",
-    }),
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			favicon: path.join(ROOT_DIR, 'favicon.ico'),
+			template: path.join(ROOT_DIR, 'index.html'),
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.EnvironmentPlugin({
+			...commonEnvConfig,
+			MSW_MODE: process.env.MSW_MODE || 'disabled',
+			SENTRY_RELEASE: '',
+		}),
+	],
 
-  optimization: {
-    moduleIds: "named",
-    splitChunks: {
-      chunks: "all",
-    },
-  },
+	optimization: {
+		moduleIds: 'named',
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
 
-  devServer: {
-    historyApiFallback: true,
-    client: { overlay: false },
-  },
+	devServer: {
+		historyApiFallback: true,
+		client: { overlay: false },
+	},
 };
 
 export default merge(commonDevAndProd, config);
