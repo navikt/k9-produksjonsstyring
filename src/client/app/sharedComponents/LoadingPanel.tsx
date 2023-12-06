b8677c4c98b66facf84d836bd8d086d6a52f0c1a
vebnor <vebjorn.nordby@nav.no>
2023-12-06 13:07:28 +0100

    legger inn loadingpanel

---
+++ src/client/app/sharedComponents/LoadingPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Loader } from '@navikt/ds-react';
+
+/**
+ * LoadingPanel
+ *
+ * Presentasjonskomponent. Viser lasteikon.
+ */
+const LoadingPanel = () => <Loader size="2xlarge" className="w-1/2" />;
+
+export default LoadingPanel;
