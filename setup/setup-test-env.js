3e4f7e1a512b2f9b28e893a6248eaddc80e0f9c0
vebnor <vebjorn.nordby@nav.no>
2023-12-06 13:49:04 +0100

    rydde

--- setup/setup-test-env.js
+++ setup/setup-test-env.js
@@ -1,24 +1,6 @@
 import configure from '@commercetools/enzyme-extensions';
 import '@testing-library/jest-dom';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import dayjs from 'dayjs';
 import durationPlugin from 'dayjs/plugin/duration';
-import { configure as configureEnzyme } from 'enzyme';
-import ShallowWrapper from 'enzyme/ShallowWrapper';
 
-configureEnzyme({ adapter: new Adapter() });
 dayjs.extend(durationPlugin);
-
-configure(ShallowWrapper);
-
-jest.mock('react-intl', () => {
-	const reactIntl = jest.requireActual('react-intl');
-	const intl = reactIntl.createIntl({
-		locale: 'nb-NO',
-	});
-
-	return {
-		...reactIntl,
-		useIntl: () => intl,
-	};
-});
