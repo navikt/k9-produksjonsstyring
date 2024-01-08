00e0b3a2364f0d93f1e871e0ab3c06a8d002f53d
vebnor <vebjorn.nordby@nav.no>
2024-01-08 15:28:51 +0100

    tilpasser tester

--- e2e/tests/ny-behandlingsko.ts
+++ e2e/tests/ny-behandlingsko.ts
@@ -1,34 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test,  } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:8030/');
   await page.getByRole('button', { name: 'Avdelingslederpanel' }).click();
-  await page.getByRole('link', { name: 'a11y-title Saksbehandlere' }).click();
-  await page.getByLabel('Epost').click();
-  await page.getByLabel('Epost').fill('saksbehandler@nav.no');
-  await page.getByRole('button', { name: 'Legg til saksbehandler' }).click();
   await page.getByRole('link', { name: 'Nye behandlingskøer' }).click();
   await page.getByRole('button', { name: 'Legg til ny behandlingskø' }).click();
   await page.getByLabel('Kønavn').fill('testkø');
   await page.getByLabel('Kønavn').press('Enter');
   await page.getByLabel('Velg saksbehandlere').click();
   await page.getByLabel('Velg saksbehandlere').fill('saks');
-  await page.getByText('saksbehandler@nav.no').click();
+  await page.getByText('Saksbehandler Sara').click();
   await page.getByRole('button', { name: 'Legg til saksbehandlere' }).click();
   await page.getByLabel('Beskrivelse').click();
   await page.getByLabel('Beskrivelse').fill('aisjdfiasjdfiasdfjasidfj');
   await page.getByRole('button', { name: 'Lagre behandlingskø' }).click();
   await page.getByTestId('lagre-button-modal').click();
   await page.getByRole('button', { name: 'Lukk' }).click();
-  // Expect a title "to contain" a substring.
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
