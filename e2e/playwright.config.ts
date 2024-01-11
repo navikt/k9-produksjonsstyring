77c35e39fb9bbe4d14742b15b2c79e41c431c845
vebnor <vebjorn.nordby@nav.no>
2024-01-11 13:25:27 +0100

    config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
-		['html', { outputFolder: './playwright-report' }],
+		['html', { outputFolder: './playwright-report/', filename: 'report.html' }],
 	] /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */,
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,7 +33,7 @@ export default defineConfig({
 
 	// serveren skal startes i k9-verdikjede
 	webServer: {
-		command: '',
+		command: 'yarn test',
 		reuseExistingServer: !!process.env.CI,
 	},
 	projects: [
--- e2e/tests/ny-behandlingsko.spec.ts
+++ e2e/tests/ny-behandlingsko.spec.ts
@@ -9,11 +9,12 @@ test('has title', async ({ page }) => {
   await page.getByLabel('Kønavn').press('Enter');
   await page.getByLabel('Velg saksbehandlere').click();
   await page.getByLabel('Velg saksbehandlere').fill('saks');
-  await page.getByText('Saksbehandler Sara').click();
+  await page.getByLabel('Saksbehandler Sara').click();
   await page.getByRole('button', { name: 'Legg til saksbehandlere' }).click();
   await page.getByLabel('Beskrivelse').click();
   await page.getByLabel('Beskrivelse').fill('aisjdfiasjdfiasdfjasidfj');
   await page.getByRole('button', { name: 'Lagre behandlingskø' }).click();
   await page.getByTestId('lagre-button-modal').click();
   await page.getByRole('button', { name: 'Lukk' }).click();
+  
 });
