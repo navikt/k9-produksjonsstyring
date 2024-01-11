5b18cb14d0e9d7f35332c1cf75ace871fafd656e
vebnor <vebjorn.nordby@nav.no>
2024-01-11 15:39:15 +0100

    må faktisk skru på video også

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'line',
 	use: {
+		video: 'on',
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: 'http://127.0.0.1:8030',
 
