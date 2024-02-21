f5c6b80c577d5f6d3696158f9ad26e57e3bcfa04
vebnor <vebjorn.nordby@nav.no>
2024-02-21 12:56:00 +0100

    legger inn backend for frontend

--- server/envVariables.js
+++ server/envVariables.js
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const envVariables = () => [
+export const envVariables = () => [
 	{
 		key: 'SAKSBEHANDLER_KAN_VELGE_NYE_KOER',
 		value: process.env.SAKSBEHANDLER_KAN_VELGE_NYE_KOER,
@@ -12,5 +8,3 @@ const envVariables = () => [
 		value: process.env.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER,
 	},
 ];
-
-export default envVariables;
