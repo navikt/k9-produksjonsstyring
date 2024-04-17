3a6e0e08f3c2dc67399198fd7baac417fbf556e6
vebnor <vebjorn.nordby@nav.no>
2024-04-17 15:19:30 +0200

    Revert "bugfix"
    This reverts commit f9db59d52d1088462800ee6dad3a4f345674689e.

--- src/client/app/avdelingsleder/reservasjoner/components/ReservasjonerTabellV1.tsx
+++ src/client/app/avdelingsleder/reservasjoner/components/ReservasjonerTabellV1.tsx
@@ -104,7 +104,7 @@ const ReservasjonerTabell = () => {
 								<Table.DataCell>{reservasjon.saksnummer}</Table.DataCell>
 								<Table.DataCell>
 									{getKodeverknavnFraKode(
-										reservasjon.behandlingstype?.kode,
+										reservasjon.behandlingstype.kode,
 										kodeverkTyper.BEHANDLING_TYPE,
 										alleKodeverk,
 									) + (reservasjon ? ' - [B] ' : '')}
