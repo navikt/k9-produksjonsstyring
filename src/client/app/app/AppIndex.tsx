622a07be45e66071976e1a6e6ba0df1ae946f04b
vebnor <vebjorn.nordby@nav.no>
2024-09-02 13:53:58 +0200

    teste verdikjede

--- src/client/app/app/AppIndex.tsx
+++ src/client/app/app/AppIndex.tsx
@@ -40,7 +40,7 @@ const AppIndex: FunctionComponent = () => {
 	const location = useLocation();
 	const queryStrings = parseQueryString(location.search);
 
-	return <>testolini </>;
+	return <>testolini 123 </>;
 };
 
 export default AppIndex;
