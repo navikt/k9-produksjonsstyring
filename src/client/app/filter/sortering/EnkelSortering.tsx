a5fa168ec1f1362dcf8831bb4dab9487856c74fe
vebnor <vebjorn.nordby@nav.no>
2024-11-04 14:45:13 +0100

    Nye sorteringsalternativer

--- src/client/app/filter/sortering/EnkelSortering.tsx
+++ src/client/app/filter/sortering/EnkelSortering.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useMemo } from 'react';
 import { Select } from '@navikt/ds-react';
 import { FilterContext } from 'filter/FilterContext';
 import { addSortering, resetSortering } from 'filter/queryUtils';
-import { mapKodeTilSorteringParams, mapSorteringParamsTilKode } from './sorteringUtils';
+import { SORTERING_ALTERNATIVER, mapKodeTilSorteringParams, mapSorteringParamsTilKode } from './sorteringUtils';
 
 const EnkelSortering = () => {
 	const { oppgaveQuery, updateQuery } = useContext(FilterContext);
@@ -18,7 +18,7 @@ const EnkelSortering = () => {
 	}, [oppgaveQuery]);
 
 	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		const payload = mapKodeTilSorteringParams(e.target.value);
+		const payload = mapKodeTilSorteringParams(e.target.value as SORTERING_ALTERNATIVER);
 		if (payload) {
 			updateQuery([resetSortering(), addSortering(payload)]);
 		}
@@ -26,8 +26,20 @@ const EnkelSortering = () => {
 
 	return (
 		<Select label="Sortering:" size="small" onChange={handleChange} value={selectValue}>
-			<option value="mottattDatoEldstTilNyest">Mottatt dato: eldste til nyeste</option>
-			<option value="mottattDatoNyestTilEldst">Mottatt dato: nyeste til eldste</option>
+			<option value={SORTERING_ALTERNATIVER.mottattDatoEldstTilNyest}>Mottatt dato: eldste til nyeste</option>
+			<option value={SORTERING_ALTERNATIVER.mottattDatoNyestTilEldst}>Mottatt dato: nyeste til eldste</option>
+			<option value={SORTERING_ALTERNATIVER.feilutbetaltBeløpSynkende}>
+				Feilutbetaling: høyeste til laveste beløp (kun tilbakekreving)
+			</option>
+			<option value={SORTERING_ALTERNATIVER.feilutbetaltBeløpØkende}>
+				Feilutbetaling: laveste til høyeste beløp (kun tilbakekreving)
+			</option>
+			<option value={SORTERING_ALTERNATIVER.feilutbetalingsdatoSynkende}>
+				Feilutbetalingsdato: eldste til nyeste (kun tilbakekreving)
+			</option>
+			<option value={SORTERING_ALTERNATIVER.feilutbetalingsdatoØkende}>
+				Feilutbetalingsdato: nyeste til eldste (kun tilbakekreving)
+			</option>
 		</Select>
 	);
 };
--- src/client/app/filter/sortering/sorteringUtils.ts
+++ src/client/app/filter/sortering/sorteringUtils.ts
@@ -1,16 +1,54 @@
-export const mapKodeTilSorteringParams = (kode: string) => {
+export enum SORTERING_ALTERNATIVER {
+	mottattDatoEldstTilNyest = 'mottattDatoEldstTilNyest',
+	mottattDatoNyestTilEldst = 'mottattDatoNyestTilEldst',
+	feilutbetaltBeløpSynkende = 'feilutbetaltBeløpSynkende',
+	feilutbetaltBeløpØkende = 'feilutbetaltBeløpØkende',
+	feilutbetalingsdatoSynkende = 'feilutbetalingsdatoSynkende',
+	feilutbetalingsdatoØkende = 'feilutbetalingsdatoØkende',
+}
+
+const koder = {
+	mottattDato: 'mottattDato',
+	førsteFeilutbetalingDato: 'førsteFeilutbetalingDato',
+	feilutbetaltBeløp: 'feilutbetaltBeløp',
+};
+export const mapKodeTilSorteringParams = (kode: SORTERING_ALTERNATIVER) => {
 	switch (kode) {
-		case 'mottattDatoEldstTilNyest':
+		case SORTERING_ALTERNATIVER.mottattDatoEldstTilNyest:
 			return {
 				kode: 'mottattDato',
 				område: 'K9',
 				økende: true,
 			};
-		case 'mottattDatoNyestTilEldst':
+		case SORTERING_ALTERNATIVER.mottattDatoNyestTilEldst:
 			return {
 				kode: 'mottattDato',
 				område: 'K9',
 				økende: false,
+			};
+		case SORTERING_ALTERNATIVER.feilutbetaltBeløpSynkende:
+			return {
+				kode: 'feilutbetaltBeløp',
+				område: 'K9',
+				økende: false,
+			};
+		case SORTERING_ALTERNATIVER.feilutbetaltBeløpØkende:
+			return {
+				kode: 'feilutbetaltBeløp',
+				område: 'K9',
+				økende: true,
+			};
+		case SORTERING_ALTERNATIVER.feilutbetalingsdatoSynkende:
+			return {
+				kode: 'førsteFeilutbetalingDato',
+				område: 'K9',
+				økende: false,
+			};
+		case SORTERING_ALTERNATIVER.feilutbetalingsdatoØkende:
+			return {
+				kode: 'førsteFeilutbetalingDato',
+				område: 'K9',
+				økende: true,
 			};
 
 		default:
@@ -20,8 +58,14 @@ export const mapKodeTilSorteringParams = (kode: string) => {
 
 export const mapSorteringParamsTilKode = ({ kode, økende }: { kode: string; økende: boolean }) => {
 	switch (kode) {
-		case 'mottattDato':
+		case koder.mottattDato:
 			return økende ? 'mottattDatoEldstTilNyest' : 'mottattDatoNyestTilEldst';
+		case koder.førsteFeilutbetalingDato:
+			return økende
+				? SORTERING_ALTERNATIVER.feilutbetalingsdatoØkende
+				: SORTERING_ALTERNATIVER.feilutbetalingsdatoSynkende;
+		case koder.feilutbetaltBeløp:
+			return økende ? SORTERING_ALTERNATIVER.feilutbetaltBeløpØkende : SORTERING_ALTERNATIVER.feilutbetaltBeløpSynkende;
 		default:
 			return null;
 	}
